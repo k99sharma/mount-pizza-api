88b1f79d7abd3da0e2917f9aa78cc177ffe9d8f8
ghost_32 <kalash.strt@gmail.com>
2022-03-14 22:10:23 +0530

    all the controllers in routes are wrapped in catchErrors handler

--- routes/orders.js
+++ routes/orders.js
@@ -2,19 +2,22 @@
 const express = require('express')
 const router = express.Router();
 
+// importing middlewares
+const { catchErrors } = require('../configs/errorHandlers');
+
 // importing controllers
 const order = require('../controllers/orders');
 
 // POST: create an new order
-router.post('/create', order.createOrder);
+router.post('/create', catchErrors(order.createOrder));
 
 // DELETE: delete order
-router.delete('/:id/delete', order.deleteOrder);
+router.delete('/:id/delete', catchErrors(order.deleteOrder));
 
 // GET: get order
-router.get('/:id/get', order.getOrder);
+router.get('/:id/get', catchErrors(order.getOrder));
 
 // PUT: update an order
-router.put('/:id/update', order.updateOrder);
+router.put('/:id/update', catchErrors(order.updateOrder));
 
 module.exports = router;

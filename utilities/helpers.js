9bc00e7ab2bc0063606c5117d85fb6ef63592c16
ghost_32 <kalash.strt@gmail.com>
2022-03-14 20:40:24 +0530

    helper function to send error and success is created

---
+++ utilities/helpers.js
@@ -0,0 +1,29 @@
+const res = require("express/lib/response");
+
+const { OK } = required('./helpers');
+
+// function to send error
+module.exports.sendError = (req, message, status ) => {
+    res.status(status).json({
+        message,
+        error: true,
+        data: null,
+    });
+}
+
+// function to send success
+module.exports.sendSuccess = (req, data, token) => {
+    if(token){
+        return res.status(OK).header("x-auth-token", token).json({
+            message: "success",
+            error: false,
+            data,
+        });
+    }
+
+    res.status(OK).json({
+        message: "success",
+        error: false,
+        data
+    });
+};
